31c445e51f1abe831de6e2f1a6a174bab4fa3b79
SalmandaScript <110246267+SalmandaScript@users.noreply.github.com>
2024-01-02 13:06:39 +0200

    Add files via upload

--- President.js
+++ President.js
@@ -257,7 +257,7 @@ open("https://salmandascript.github.io/AboutSalmanda/contact.html");
 }
 
 //function to open Terms of service 
-function termsOfService(){
+function Service(){
 open("https://salmandascript.github.io/AboutSalmanda/Service.html");
 }
 
